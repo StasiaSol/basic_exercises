0d997acef319d0f27834469213c583c773d0b8ea
Stasia Soloduha <teoretic.nest@gmail.com>
2022-09-16 23:46:07 +0300

    Learn-homework 3 week

--- for_dict_challenges.py
+++ for_dict_challenges.py
@@ -1,78 +1,152 @@
-# Задание 1
-# Дан список учеников, нужно посчитать количество повторений каждого имени ученика
-# Пример вывода:
-# Вася: 1
-# Маша: 2
-# Петя: 2
-
-students = [
-    {'first_name': 'Вася'},
-    {'first_name': 'Петя'},
-    {'first_name': 'Маша'},
-    {'first_name': 'Маша'},
-    {'first_name': 'Петя'},
-]
-# ???
+def get_count_text(students):
+    new_list = []
+    text_count = ""
+    for student in students:
+        if student in new_list:
+            continue
+        else:
+            text_count += (
+                student["first_name"] + ": " + str(students.count(student)) + "\n"
+            )
+            new_list.append(student)
+    return text_count
 
 
-# Задание 2
-# Дан список учеников, нужно вывести самое часто повторящееся имя
-# Пример вывода:
-# Самое частое имя среди учеников: Маша
-students = [
-    {'first_name': 'Вася'},
-    {'first_name': 'Петя'},
-    {'first_name': 'Маша'},
-    {'first_name': 'Маша'},
-    {'first_name': 'Оля'},
-]
-# ???
+def get_max_count(students):
+    max_count = 0
+    for student in students:
+        count_student = students.count(student)
+        if count_student > max_count:
+            element_list = student
+            max_count = count_student
+    return element_list["first_name"]
 
 
-# Задание 3
-# Есть список учеников в нескольких классах, нужно вывести самое частое имя в каждом классе.
-# Пример вывода:
-# Самое частое имя в классе 1: Вася
-# Самое частое имя в классе 2: Маша
+def count_girl_boy(school, is_male):
+    text_result = ""
+    for classes in school:
+        text_result += "Класс " + classes["class"] + ":"
+        count_girls = 0
+        count_boys = 0
+        for student in classes["students"]:
+            if is_male[student["first_name"]]:
+                count_boys += 1
+            else:
+                count_girls += 1
+        text_result += (
+            " девочки " + str(count_girls) + ", мальчики " + str(count_boys) + "\n"
+        )
+    return text_result
 
-school_students = [
-    [  # это – первый класс
-        {'first_name': 'Вася'},
-        {'first_name': 'Вася'},
-    ],
-    [  # это – второй класс
-        {'first_name': 'Маша'},
-        {'first_name': 'Маша'},
-        {'first_name': 'Оля'},
-    ],[  # это – третий класс
-        {'first_name': 'Женя'},
-        {'first_name': 'Петя'},
-        {'first_name': 'Женя'},
-        {'first_name': 'Саша'},
-    ],
-]
-# ???
+
+def max_count_students(school, is_male):
+    list_result = ["Больше всего мальчиков в классе", "Больше всего девочек в классе"]
+    for classes in school:
+        count_girls = 0
+        count_boys = 0
+        for student in classes["students"]:
+            if is_male[student["first_name"]]:
+                count_boys += 1
+            else:
+                count_girls += 1
+        if count_boys > count_girls:
+            list_result[0] += classes["class"] + " "
+        else:
+            list_result[1] += classes["class"] + " "
+    return list_result[0] + "\n" + list_result[1]
+
+
+if __name__ == "__main__":
+
+    # Задание 1
+    # Дан список учеников, нужно посчитать количество повторений каждого имени ученика
+    # Пример вывода:
+    # Вася: 1
+    # Маша: 2
+    # Петя: 2
+
+    students = [
+        {"first_name": "Вася"},
+        {"first_name": "Петя"},
+        {"first_name": "Маша"},
+        {"first_name": "Маша"},
+        {"first_name": "Петя"},
+    ]
+    print(get_count_text(students))
+    # ???
+
+    # Задание 2
+    # Дан список учеников, нужно вывести самое часто повторящееся имя
+    # Пример вывода:
+    # Самое частое имя среди учеников: Маша
+    students = [
+        {"first_name": "Вася"},
+        {"first_name": "Петя"},
+        {"first_name": "Маша"},
+        {"first_name": "Маша"},
+        {"first_name": "Оля"},
+    ]
+    # ???
+    print("Самое частое имя среди учеников: " + get_max_count(students))
+
+    # Задание 3
+    # Есть список учеников в нескольких классах, нужно вывести самое частое имя в каждом классе.
+    # Пример вывода:
+    # Самое частое имя в классе 1: Вася
+    # Самое частое имя в классе 2: Маша
+
+    school_students = [
+        [  # это – первый класс
+            {"first_name": "Вася"},
+            {"first_name": "Вася"},
+        ],
+        [  # это – второй класс
+            {"first_name": "Маша"},
+            {"first_name": "Маша"},
+            {"first_name": "Оля"},
+        ],
+        [  # это – третий класс
+            {"first_name": "Женя"},
+            {"first_name": "Петя"},
+            {"first_name": "Женя"},
+            {"first_name": "Саша"},
+        ],
+    ]
+    for class_students in school_students:
+        print(
+            "Самое частое имя в классе "
+            + str(school_students.index(class_students) + 1)
+            + ": "
+            + get_max_count(students)
+        )
 
 
 # Задание 4
 # Для каждого класса нужно вывести количество девочек и мальчиков в нём.
 # Пример вывода:
-# Класс 2a: девочки 2, мальчики 0 
+# Класс 2a: девочки 2, мальчики 0
 # Класс 2б: девочки 0, мальчики 2
 
 school = [
-    {'class': '2a', 'students': [{'first_name': 'Маша'}, {'first_name': 'Оля'}]},
-    {'class': '2б', 'students': [{'first_name': 'Олег'}, {'first_name': 'Миша'}]},
-    {'class': '2б', 'students': [{'first_name': 'Даша'}, {'first_name': 'Олег'}, {'first_name': 'Маша'}]},
+    {"class": "2a", "students": [{"first_name": "Маша"}, {"first_name": "Оля"}]},
+    {"class": "2б", "students": [{"first_name": "Олег"}, {"first_name": "Миша"}]},
+    {
+        "class": "2б",
+        "students": [
+            {"first_name": "Даша"},
+            {"first_name": "Олег"},
+            {"first_name": "Маша"},
+        ],
+    },
 ]
 is_male = {
-    'Олег': True,
-    'Маша': False,
-    'Оля': False,
-    'Миша': True,
-    'Даша': False,
+    "Олег": True,
+    "Маша": False,
+    "Оля": False,
+    "Миша": True,
+    "Даша": False,
 }
-# ???
+print(count_girl_boy(school, is_male))
 
 
 # Задание 5
@@ -82,14 +156,13 @@ is_male = {
 # Больше всего девочек в классе 2a
 
 school = [
-    {'class': '2a', 'students': [{'first_name': 'Маша'}, {'first_name': 'Оля'}]},
-    {'class': '3c', 'students': [{'first_name': 'Олег'}, {'first_name': 'Миша'}]},
+    {"class": "2a", "students": [{"first_name": "Маша"}, {"first_name": "Оля"}]},
+    {"class": "3c", "students": [{"first_name": "Олег"}, {"first_name": "Миша"}]},
 ]
 is_male = {
-    'Маша': False,
-    'Оля': False,
-    'Олег': True,
-    'Миша': True,
+    "Маша": False,
+    "Оля": False,
+    "Олег": True,
+    "Миша": True,
 }
-# ???
-
+print(max_count_students(school, is_male))
--- string_challenges.py
+++ string_challenges.py
@@ -1,28 +1,65 @@
+def get_last_letter(word):
+    return word[-1]
+
+
 # Вывести последнюю букву в слове
-word = 'Архангельск'
-# ???
+word = "Архангельск"
+print(get_last_letter(word))
+
+
+def get_count_letter(word, letter="а"):
+    return word.count(letter)
 
 
 # Вывести количество букв "а" в слове
-word = 'Архангельск'
-# ???
+word = "Архангельск"
+print(get_count_letter(word))
+
+
+def get_count_vowel(word):
+    count_vowel = 0
+    vowels = ["а", "е", "ё", "ы", "я", "у", "и", "о", "э", "ю"]
+    for vowel in vowels:
+        count_vowel += get_count_letter(word, vowel)
+    return count_vowel
 
 
 # Вывести количество гласных букв в слове
-word = 'Архангельск'
-# ???
+word = "Архангельск"
+print(get_count_vowel(word.lower()))
+
+
+def get_words_sentence(sentence):
+    return sentence.split()
 
 
 # Вывести количество слов в предложении
-sentence = 'Мы приехали в гости'
-# ???
+sentence = "Мы приехали в гости"
+print(len(get_words_sentence(sentence)))
+
+
+def get_first_letters(sentence):
+    text = ""
+    for word in get_words_sentence(sentence):
+        text += word[0] + "\n"
+    return text
 
 
 # Вывести первую букву каждого слова на отдельной строке
-sentence = 'Мы приехали в гости'
-# ???
+sentence = "Мы приехали в гости"
+print(get_first_letters(sentence))
+
+
+def get_middle_len_word(sentence):
+    middle_len = 0
+    count_len = 0
+    list_words = get_words_sentence(sentence)
+    for word in list_words:
+        count_len += len(word)
+    middle_len = count_len / len(list_words)
+    return str(middle_len)
 
 
 # Вывести усреднённую длину слова в предложении
-sentence = 'Мы приехали в гости'
-# ???
+sentence = "Мы приехали в гости"
+print(get_middle_len_word(sentence))
